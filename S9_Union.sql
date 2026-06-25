a04f50fa246ed12ed13400810b9165e4418b853a
Sachi <kotiansachi@gmail.com>
2026-06-25 12:45:46 +0530

    Important SQL topics covered

---
+++ S9_Union.sql
@@ -0,0 +1,34 @@
+USE USER;
+
+CREATE TABLE admin_users (
+id INT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    gender ENUM('Male', 'Female', 'Other'),
+    date_of_birth DATE,
+    salary INT
+);
+
+INSERT INTO admin_users (id, name, email, gender, date_of_birth, salary) VALUES
+(101, 'Anil Kumar', 'anil@example.com', 'Male', '1985-04-12', 60000),
+(102, 'Pooja Sharma', 'pooja@example.com', 'Female', '1992-09-20', 58000),
+(103, 'Rakesh Yadav', 'rakesh@example.com', 'Male', '1989-11-05', 54000),
+(104, 'Fatima Begum', 'fatima@example.com', 'Female', '1990-06-30', 62000);
+
+SELECT NAME FROM USERS
+UNION
+SELECT NAME FROM ADMIN_USERS;
+
+SELECT NAME FROM USERS
+UNION ALL
+SELECT NAME FROM ADMIN_USERS;
+
+-- ADDING ROLES
+SELECT NAME,'USER' AS ROLE FROM USERS
+UNION
+SELECT NAME,'ADMIN' AS ROLE FROM ADMIN_USERS;
+
+SELECT NAME FROM USERS
+UNION
+SELECT NAME FROM ADMIN_USERS
+ORDER BY NAME; 
